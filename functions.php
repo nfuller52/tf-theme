6cf20a8d44830d85bdc864a1c8af836c66516e2c
Nick Fuller <nfuller52@gmail.com>
2015-08-22 21:57:57 -0700

    WIP: extract css into scss partials

--- functions.php
+++ functions.php
@@ -1,11 +1,16 @@
 <?php
+//* Start the engine!
 include_once( get_template_directory() . '/lib/init.php' );
 
 define( 'CHILD_THEME_NAME', 'TasukuForce' );
 define( 'CHILD_THEME_URL', 'http://www.tasukuforce.com' );
 define( 'CHILD_TEHME_VERSION', '0.0.1' );
 
-add_theme_support( 'html5', array( 'search-form' ) );
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+
+//* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
-add_theme_support( 'custom-background' );
+
+//* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
